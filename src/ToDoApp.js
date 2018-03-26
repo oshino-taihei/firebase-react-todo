0f225fe744e7fda99511074e8d621f7ec728f48b
押野泰平 <toshino@oshinotaiheinoMacBook-Pro.local>
2018-03-26 14:04:57 +0900

    タスクを削除できるようにした

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -3,8 +3,10 @@ import CssBaseline from 'material-ui/CssBaseline'
 import AppBar from 'material-ui/AppBar';
 import Input from 'material-ui/Input'
 import Button from 'material-ui/Button'
+import IconButton from 'material-ui/IconButton'
 import Typography from 'material-ui/Typography';
-import List, { ListItem, ListItemText } from 'material-ui/List'
+import List, { ListItem, ListItemText, ListItemSecondaryAction } from 'material-ui/List'
+import DeleteIcon from 'material-ui-icons/Delete'
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 import { database } from './firebase-database'
@@ -17,40 +19,59 @@ class ToDoApp extends Component {
             tasks: [],
             task: ''
         }
-        this.loadDatabase()
+        this.initTasksRef()
     }
 
-    inputTask = task => {
+    initTasksRef = () => {
+        this.tasksRef = database.ref('tasks')
+        this.tasksRef.off()
+        this.tasksRef.on('child_added', data => {
+            const task = data.val().task
+            this.setState({
+                tasks: [ ...this.state.tasks, task],
+                task: ''
+            })
+        })
+        this.tasksRef.on('child_removed', data => {
+            const task = data.val().task
+            const index = this.state.tasks.indexOf(task)
+            if (index !== -1) {
+                this.state.tasks.splice(index, 1)
+                this.setState({ tasks: this.state.tasks })
+            }
+        })
+    }
+
+    inputTaskHandler = task => {
         this.setState({
             task: task
         })
     }
 
     addTask = task => {
-        const tasksRef = database.ref('tasks');
-        tasksRef.push({
+        this.tasksRef.push({
             task: task
         }).catch(error => {
             console.error('Error writing new task to Firebase Database', error);
         })
     }
 
-    loadDatabase = () => {
-        const tasksRef = database.ref('tasks')
-        tasksRef.off()
-        tasksRef.on('child_added', data => {
-            const task = data.val().task
-            this.setState({
-                tasks: [ ...this.state.tasks, task],
-            })
+    deleteTask = task => {
+        this.tasksRef.orderByChild('task').equalTo(task).once('child_added', data => {
+            this.tasksRef.child(data.key).remove()
         })
     }
 
     render() {
-        const listItems = this.state.tasks.map((item, i) => {
+        const listItems = this.state.tasks.map((task, i) => {
             return (
                 <ListItem key={i}>
-                    <ListItemText primary={`・${item}`} />
+                    <ListItemText primary={task} />
+                    <ListItemSecondaryAction>
+                        <IconButton aria-label="削除">
+                        <DeleteIcon onClick={e => this.deleteTask(task)} />
+                        </IconButton>
+                    </ListItemSecondaryAction>
                 </ListItem>
             )
         })
@@ -64,7 +85,7 @@ class ToDoApp extends Component {
                 </Typography>
             </AppBar>
             <div style={{ padding: '16px' }}>
-                <Input onChange={ e => this.inputTask(e.target.value) } />
+                <Input value={ this.state.task } onChange={ e => this.inputTaskHandler(e.target.value) } />
                 <Button color="secondary" onClick={ () => this.addTask(this.state.task) } >登録</Button>
                 <List>
                     <CSSTransitionGroup
